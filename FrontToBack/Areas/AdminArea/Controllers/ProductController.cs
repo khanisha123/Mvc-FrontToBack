38e105ae28ff3d25529a04045426e12545937d8e
khanisha123 <khanisha@code.edu.az>
2022-01-19 02:09:02 +0400

    Fixed

--- FrontToBack/Areas/AdminArea/Controllers/ProductController.cs
+++ FrontToBack/Areas/AdminArea/Controllers/ProductController.cs
@@ -125,7 +125,13 @@ namespace FrontToBack.Areas.AdminArea.Controllers
             if (id == null) return NotFound();
             Product dbProduct = await _context.Products.FindAsync(id);
             if (dbProduct == null) return NotFound();
-
+            List<SelectListItem> categories = (from i in _context.Categories.ToList()
+                                               select new SelectListItem
+                                               {
+                                                   Text = i.Name,
+                                                   Value = i.Id.ToString()
+                                               }).ToList();
+            ViewBag.dgr = categories;
             return View(dbProduct);
         }
         [HttpPost]
@@ -134,35 +140,50 @@ namespace FrontToBack.Areas.AdminArea.Controllers
         {
 
             if (id == null) return NotFound();
+
+            if (!ModelState.IsValid) return View();
+            bool isExist = _context.Products.Any(c => c.Name.ToLower() == product.Name.ToLower().Trim());
+
+            Product isExistProduct = _context.Products.FirstOrDefault(p => p.Id == product.Id);
+
+            if (isExist && !(isExistProduct.Name.ToLower() == product.Name.ToLower().Trim()))
+            {
+                ModelState.AddModelError("Name", "Bu adla product var");
+                return View();
+            };
+
+
+
             if (product.ProductPhoto != null)
             {
-
                 if (ModelState["ProductPhoto"].ValidationState == Microsoft.AspNetCore.Mvc.ModelBinding.ModelValidationState.Invalid)
                 {
-                    ModelState.AddModelError("ProductPhoto", "Do not Empty");
-
+                    ModelState.AddModelError("ProductPhoto", "Do not empty");
                 }
+
                 if (!product.ProductPhoto.IsImage())
                 {
-                    ModelState.AddModelError("ProductPhoto", "Only Image");
+                    ModelState.AddModelError("ProductPhoto", "only image");
                     return View();
                 }
                 if (product.ProductPhoto.IsCorrectSize(300))
                 {
-                    ModelState.AddModelError("ProductPhoto", "300 den yuxari ola bilmez");
+                    ModelState.AddModelError("ProductPhoto", "300den yuxari ola bilmez");
                     return View();
                 }
-
-                Product dbProduct = await _context.Products.FindAsync(id);
-                string fileName = await product.ProductPhoto.SaveImageAsync(_env.WebRootPath, "img");
-
-                string path = Path.Combine(_env.WebRootPath, "img", dbProduct.ImageUrl);
+                Product dbproduct = await _context.Products.FindAsync(id);
+                string path = Path.Combine(_env.WebRootPath, "img", dbproduct.ImageUrl);
                 if (System.IO.File.Exists(path))
                 {
                     System.IO.File.Delete(path);
                 }
+                string fileName = await product.ProductPhoto.SaveImageAsync(_env.WebRootPath, "img");
 
-                dbProduct.ImageUrl = fileName;
+                dbproduct.Category = product.Category;
+                dbproduct.Name = product.Name;
+                dbproduct.Price = product.Price;
+                dbproduct.ImageUrl = fileName;
+                dbproduct.CategoryId = product.CategoryId;
                 await _context.SaveChangesAsync();
             }
             return RedirectToAction("Index");
