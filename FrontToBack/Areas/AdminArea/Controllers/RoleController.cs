26d4f842bad10c1de3d669545e2dab218aa822b1
khanisha123 <khanisha@code.edu.az>
2022-01-27 00:55:49 +0400

    Add Roles Create and Delete

---
+++ FrontToBack/Areas/AdminArea/Controllers/RoleController.cs
@@ -0,0 +1,58 @@
+﻿using FrontToBack.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBack.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class RoleController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RoleController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+        public IActionResult Index()
+        {
+            var roles = _roleManager.Roles.ToList();
+            return View(roles);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult>  Create(string role) 
+        {
+            if (!string.IsNullOrEmpty(role))
+            {
+                if (!await _roleManager.RoleExistsAsync(role))
+                {
+                    await _roleManager.CreateAsync(new IdentityRole(role));
+                }
+                return RedirectToAction("Index");
+            }
+            return NotFound();
+        }
+        public async Task<IActionResult> DeleteRole(string Id)
+        {
+            var roleD = await _roleManager.FindByIdAsync(Id);
+            await _roleManager.DeleteAsync(roleD);
+
+            return RedirectToAction(nameof(Index));
+
+        }
+    }
+}
--- FrontToBack/Areas/AdminArea/Controllers/UserController.cs
+++ FrontToBack/Areas/AdminArea/Controllers/UserController.cs
@@ -87,5 +87,7 @@ namespace FrontToBack.Areas.AdminArea.Controllers
 
             return RedirectToAction("Index", "User");
         }
+
+        
     }
 }
